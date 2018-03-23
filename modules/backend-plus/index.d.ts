a8b92a017a716dfe7989911b5ec4ce7d2238d279
Emilio <emilioplatzer@gmail.com>
2018-03-23 13:22:10 -0300

    más tipos bp

--- modules/backend-plus/index.d.ts
+++ modules/backend-plus/index.d.ts
@@ -1,6 +1,33 @@
 declare module 'backend-plus'{
+    import * as express from "express";
+
+    interface ProcedureDef{
+
+    }
+    interface MenuInfoBase{
+        menuType:string // 'menu'|'table'|'proc'
+        name:string
+        label?:string
+    }
+    interface MenuInfoMenu extends MenuInfoBase{
+        menuType:'menu'
+        content:MenuInfo[]
+    }
+    interface MenuInfoTable extends MenuInfoBase{
+        menuType:'table'
+        table?:string
+    }
+    interface MenuInfoProc extends MenuInfoBase{
+        menuType:'proc'
+    }
+    type MenuInfo = MenuInfoBase; // MenuInfoMenu | MenuInfoTable | MenuInfoProc;
+
     class AppBackend{
+        app:express.Express
         start():Promise<void>
         getTables():string[]
+        addLoggedServices():void
+        getProcedures():Promise<ProcedureDef[]>
+        getMenu(context?:{}):{menu:MenuInfo[]}
     }
 }
