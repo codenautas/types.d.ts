297a03470235321ab86a967506a8b65e687adb4c
Emilio <emilioplatzer@gmail.com>
2018-04-06 17:01:57 -0300

    agrego más funciones para discrepances y generalizo

--- modules/discrepances/index.d.ts
+++ modules/discrepances/index.d.ts
@@ -1,4 +1,6 @@
 declare module 'discrepances'{
-    function showAndThrow(obtained:any, expected:any):void
+    function showAndThrow<T>(obtained:T, expected:T):void
+    function nestedObject<T>(obtained:T, expected:T):object
+    function flatten     <T>(obtained:T, expected:T):object
 }
 
