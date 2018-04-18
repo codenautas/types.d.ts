5ed2e807031cb424b1ce1ebb94628007e661decb
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-04-18 12:03:09 -0300

    formate code

--- modules/myOwn/index.d.ts
+++ modules/myOwn/index.d.ts
@@ -1,40 +1,40 @@
-declare module "myOwn"{
+declare module "myOwn" {
 
-    function myOwn():void
-    namespace myOwn{
-        interface AddrParams{
-            w?:'menu'|'table'|'proc'|'path'|'function'
-            i?:string
-            name?:string
-            proc?:string
-            path?:string
-            pageTitle?:string
-            title?:string
+    function myOwn(): void
+    namespace myOwn {
+        interface AddrParams {
+            w?: 'menu' | 'table' | 'proc' | 'path' | 'function'
+            i?: string
+            name?: string
+            proc?: string
+            path?: string
+            pageTitle?: string
+            title?: string
         }
-        interface WScreens{
-            [key:string]:(addrParams:AddrParams)=>void
+        interface WScreens {
+            [key: string]: (addrParams: AddrParams) => void
         }
-        interface ClientSideDefinition{
-            update?:true|((depot:Depot, fieldName:string)=>void)
-            prepare:(depot:Depot, fieldName:string)=>void
+        interface ClientSideDefinition {
+            update?: true | ((depot: Depot, fieldName: string) => void)
+            prepare: (depot: Depot, fieldName: string) => void
         }
-        interface ClientSides{
-            [key:string]:ClientSideDefinition
+        interface ClientSides {
+            [key: string]: ClientSideDefinition
         }
-        interface Depot{
-            rowControls:{[key:string]:HTMLElement}
-            row:{[key:string]:any}
+        interface Depot {
+            rowControls: { [key: string]: HTMLElement }
+            row: { [key: string]: any }
         }
-        var wScreens:WScreens
-        var clientSides:ClientSides
-        var path:{
-            img:string
+        var wScreens: WScreens
+        var clientSides: ClientSides
+        var path: {
+            img: string
         }
-        function getRect(control:HTMLElement):{
-            top:number,
-            left:number
+        function getRect(control: HTMLElement): {
+            top: number,
+            left: number
         }
-        function createForkeableButton(object:object):HTMLButtonElement
+        function createForkeableButton(object: object): HTMLButtonElement
     }
     export = myOwn
 }
