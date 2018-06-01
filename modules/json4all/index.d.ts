124849a4afdfa7f57334dd6da6cbc00217eb5705
Emilio Platzer <emilioplatzer@gmail.com>
2018-06-01 18:52:20 -0300

    memory store y json4all

---
+++ modules/json4all/index.d.ts
@@ -0,0 +1,7 @@
+declare module "json4all"{
+    namespace likeAr{
+        function parse(jsonText:string):any
+        function stringify(object:any):string
+    }
+    export = likeAr
+}
--- modules/memorystore/index.d.ts
+++ modules/memorystore/index.d.ts
@@ -1,3 +1,21 @@
 declare module 'memorystore' {
-    
+    function memorystore(session:any):typeof memorystore.MemoryStore
+    namespace memorystore{
+        type MemoryConsOpts={
+            checkPeriod?:number
+            max?:number
+            ttl?:number
+            dispose:(key:string, value:any)=>void
+            stale?:number
+            // serializer:typeof JSON
+        }
+        class MemoryStore{
+            constructor(opts:MemoryConsOpts)
+            store:{
+                load(content:any):void
+                dump():any
+            }
+        }
+    }
+    export = memorystore
 }
