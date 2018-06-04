88f3a4f92e0255efc62d124f552b9793dfd48f4d
Emilio Platzer <emilioplatzer@gmail.com>
2018-06-03 22:06:19 -0300

    json4all parse<T>

--- modules/json4all/index.d.ts
+++ modules/json4all/index.d.ts
@@ -1,6 +1,6 @@
 declare module "json4all"{
     namespace json4all{
-        function parse(jsonText:string):any
+        function parse<T>(jsonText:string):T
         function stringify(object:any):string
     }
     export = json4all
