81b6991d8fe888cd5e502fb9a7d0b4a9ec26d71a
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-06-26 12:26:14 -0300

    mueve declaration file discrepances a su propio package

--- modules/discrepances/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'discrepances'{
-    function showAndThrow<T>(obtained:T, expected:T, opts?: object):void
-    function nestedObject<T>(obtained:T, expected:T, opts?: object):object
-    function flatten     <T>(obtained:T, expected:T, opts?: object):object
-}
-
