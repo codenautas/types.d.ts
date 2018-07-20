c1f1c5589b6bb3ea37da08d176c06ff62eff5520
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-07-20 15:33:46 -0300

    completando tipos myOwn

--- modules/myOwn/index.d.ts
+++ modules/myOwn/index.d.ts
@@ -1,9 +1,11 @@
+
 declare module "myOwn" {
 
     function myOwn(): void
     namespace myOwn {
         var firstDisplayCount:number
         var i18n:any
+
         interface AddrParams {
             w?: 'menu' | 'table' | 'proc' | 'path' | 'function'
             i?: string
@@ -23,15 +25,41 @@ declare module "myOwn" {
         interface ClientSides {
             [key: string]: ClientSideDefinition
         }
+
+        interface TableGrid{
+            grid: {
+                [key:string]: any
+                dom: {main: HTMLElement, aggregate: {[key:string]: HTMLElement} }
+                modes: {saveByField: boolean, withColumnDetails: any[] }//FieldDefinition[]} // debería ser FieldDefinition pero ese tipo está declarado en BEPlus y esto (myOwn) está en types.d.ts/modules/myOwn
+                view: any
+            }
+            retrieveRowAndRefresh: (depot: Depot) => Promise<void>
+        }
+
         interface Depot {
             rowControls: { [key: string]: HTMLElement }
             row: { [key: string]: any }
+            manager: TableGrid
         }
+        
+        interface ProcedureParameters {
+            [key:string]:string | number
+        }
+
+        interface AttrFunc {
+            [key: string]: (params:ProcedureParameters) => any | {[key: string]: AttrFunc}
+        } 
+        interface MyAjax {
+            tables: (()=> void)[]
+            [key: string]: AttrFunc
+        } // ajax = {tabla_datos: {generar: ()=>any}} // return ajax.tabla_datos.generar
         var wScreens: WScreens
         var clientSides: ClientSides
         var path: {
             img: string
         }
+        var ajax: MyAjax
+
         function getRect(control: HTMLElement): {
             top: number,
             left: number
