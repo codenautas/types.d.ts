3f28d0c48fb4fc1d6bcee9bd322b59ae5ee46dfd
Emilio <emilioplatzer@gmail.com>
2019-05-14 13:08:47 -0300

    mejoro typed-controls

--- modules/typed-controls/index.d.ts
+++ modules/typed-controls/index.d.ts
@@ -1,6 +1,12 @@
 declare module "typed-controls" {
-    type TypedControlBase<T>={
-        setTypedValue(value:T):void
+    type TypedControlBase<T>={} | {
+        controledType:{typeInfo:{
+            valueNoData?:string
+            valueUnknownData?:string
+        }}
+        setTypedValue(value:T, fromUserInteraction?:boolean):void
+        getTypedValue:()=>any,
+        disable:(disabled?:boolean)=>void,
     }
     type TypedControl<T>=TypedControlBase<T> & HTMLElement;
     function adaptElement<T>(control:TypedControl<T>, typeInfo:any):void
