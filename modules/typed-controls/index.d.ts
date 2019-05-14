b30c258d4d6ddcd14eaaedf4a6ca860da00dd438
Emilio <emilioplatzer@gmail.com>
2019-05-14 15:23:52 -0300

    avanzo con typed controls

--- modules/typed-controls/index.d.ts
+++ modules/typed-controls/index.d.ts
@@ -1,12 +1,18 @@
 declare module "typed-controls" {
     type TypedControlBase<T>={} | {
-        controledType:{typeInfo:{
-            valueNoData?:string
-            valueUnknownData?:string
-        }}
+        controledType:{
+            typeInfo:{
+                valueNoData?:string
+                valueUnknownData?:string
+                
+            }
+            isValidTypedData(value:T):boolean
+            fromPlainJson(json:string):T
+            toPlainJson(value:T):string
+        }
         setTypedValue(value:T, fromUserInteraction?:boolean):void
-        getTypedValue:()=>any,
-        disable:(disabled?:boolean)=>void,
+        getTypedValue():T
+        disable(disabled?:boolean):void
     }
     type TypedControl<T>=TypedControlBase<T> & HTMLElement;
     function adaptElement<T>(control:TypedControl<T>, typeInfo:any):void
