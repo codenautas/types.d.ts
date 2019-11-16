332fd0dd7c01b7d209b64ec265832ce3f3b2313b
Emilio Platzer <emilioplatzer@gmail.com>
2019-11-16 20:20:31 -0300

    Agrego info de typestore

--- modules/type-store/index.d.ts
+++ modules/type-store/index.d.ts
@@ -1,3 +1,20 @@
 declare module 'type-store' {
-    
+    type TypeInfo={
+        typeName:'text'|'number'|'integer'|'bigint'|'double'|'boolean'|'date'|'timestamp'|'interval'
+        nullable?:boolean
+        acceptedChars?:RegExp
+        postInput?:string
+    }|{
+        typeName:'text'
+        allowEmptyText?:boolean
+        toUpperWithoutDiacritics?:boolean
+    }|{
+        typeName:'interval'
+        timeUnit?:string
+    }
+    type TypeBase<T>={
+        toHtmlText():string
+        toHtml(value:T):HtmlBase
+    }
+    function typerFrom(typeInfo:{typeName:'date'}&TypeInfo):TypeBase<Date>
 }
